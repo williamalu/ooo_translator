fb5d14942d77f47b8f7642509fe8c08b9ed1c6a8
William Lu <william.a.lu@gmail.com>
2016-01-02 18:22:42 -0800

    added comments

--- ooo_converter.py
+++ ooo_converter.py
@@ -23,29 +23,30 @@ def isconsonant(letter):
             return True
     return False
 
-def convert_oo(temp_text):
+
+def convert_oo(source_text):
     """
     Converts given text into Ooo language.
     """
+    temp_text = source_text.split(' ') #splits orignal string into list of words
 
-    temp_text = temp_text.split(' ')
+    #replaces all vowels with o or O, depending on capitalization
     for i in range(len(temp_text)):
         for v in vowels:
             temp_text[i] = temp_text[i].replace(v, 'o')
         for V in capvowels:
             temp_text[i] = temp_text[i].replace(V, 'O')
 
+    #for readability, turns all singular "o"s into "oo"
     for i in range(len(temp_text)):
-        last_pos = 0;
+        last_pos = 0; #last position of "o" converted to "oo"
 
-        # print "word number %i" %i
-
+        #if there are any remaining "o"s left in the word, continue parsing for and replacing them
         while 'o' in temp_text[i].replace('oo', ''):
 
-            pos = temp_text[i].find('o', last_pos)
-            # print ' '.join(temp_text)
-            # print pos
+            pos = temp_text[i].find('o', last_pos) #begin searching for "o"s at the last location an "o" was found
 
+            #catch single letter words and convert them to "oo" if they are vowels
             if len(temp_text[i]) == 1:
                 if isconsonant(temp_text[i]) == True:
                     break
@@ -53,34 +54,41 @@ def convert_oo(temp_text):
                     temp_text[i] = 'oo'
                     break
 
+            #catches if trying to index beyond the length of a string
+            #if the last letter of a word is an "o" and the second to last letter is a consonant, convert the "o" to "oo"
             if (pos + 1) >= len(temp_text[i]):
                 if isconsonant(temp_text[i][pos - 1]) == True:
                     temp_list = list(temp_text[i])
                     temp_list[pos] = 'oo'
                     temp_text[i] = ''.join(temp_list)
-                    last_pos = 0
+                    last_pos = 0 #reset the last_pos counter
                     break
 
+            #catches if trying to index before the first letter of a string
+            #if the first letter of a word is an "o" and the second letter is a consonant, conver the "o" to "oo"
             if (pos - 1) < 0:
                 if isconsonant(temp_text[i][pos + 1]) == True:
                     temp_list = list(temp_text[i])
                     temp_list[pos] = 'oo'
                     temp_text[i] = ''.join(temp_list)
-                    last_pos = pos + 2
+                    last_pos = pos + 2 #set last_pos counter to after the newly inserted "oo"
                     continue
 
+            #if there is already an "oo" in the string, search for another "o" after the "oo"
             if temp_text[i][pos + 1] == 'o':
-                last_pos = pos + 2
+                last_pos = pos + 2 #set last_pos counter to after the newly inserted "oo"
                 continue
 
+            #if there is an "o" between two consonants, convert the "o" to "oo"
             if (isconsonant(temp_text[i][pos - 1]) == True) and (isconsonant(temp_text[i][pos + 1]) == True):
                 temp_list = list(temp_text[i])
                 temp_list[pos] = 'oo'
                 temp_text[i] = ''.join(temp_list)
-                last_pos = pos + 2
-                if last_pos > len(temp_text):
+                last_pos = pos + 2 #set last_pos counter to after the newly inserted "oo"
+                if last_pos > len(temp_text): #catch if trying to index beyond the length of a string
                     break
 
+    #searches for known ooo conversions for names and substitutes proper conversions
     for i in range(len(temp_text)):
         for n in names:
             if temp_text[i] == n:
@@ -88,11 +96,11 @@ def convert_oo(temp_text):
 
     return ' '.join(temp_text)
 
-# print convert_oo('Alex Bryan Celina Daniel Eric Franton Jennifer Jenny Katie Kim Lauren Linnea Michael Tatiana William')
-
-print convert_oo("nice bow tie")
-
-
-#first, convert all consonants to 'o'
-#second, check for names and convert the rest
-#third, if single vowel, use 'oo' instead of 'o'
+if __name__ == '__main__':
+    print convert_oo('Alex Bryan Celina Daniel Eric Franton Jennifer Jenny Katie Kim Lauren Linnea Michael Tatiana William')
+    print convert_oo("Hi everyone!")
+    print convert_oo("Much like flying makes Baymax a better healthcare companion, bonding events make us a better group.  Before everyone fills up their semester with (gasp!) meetings, let's decide on a date where we'll all go off campus to do something fun.  This will also be a great chance for us to introduce Daniel to the collective weirdness that is PowerChords!")
+    print convert_oo("Here is the whenisgood:")
+    print convert_oo("Here is a survey on what we should do:")
+    print convert_oo("Happy New Year!")
+    print convert_oo("William")
